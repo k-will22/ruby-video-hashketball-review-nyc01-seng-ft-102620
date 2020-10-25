e12a586ece69424fc3b524257f2ff0f9ec14a690
Keith Williams <thestreetkw@hotmail.com>
2020-10-25 04:01:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,9 +191,8 @@ def big_shoe_rebounds
   largest_shoe = {}
   players.each do |i|
     if i[:shoe] > index 
-        puts largest_shoe = i
+        largest_shoe = i[:rebounds]
     end 
  end
- largest_shoe[:rebounds]
- binding.pry
+ largest_shoe 
 end 
